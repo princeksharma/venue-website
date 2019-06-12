4fd941d240bad0b2221d7c5cdea85e8d6f241b85
Prince Sharma <sharmaprince2271997@gmail.com>
2019-06-12 18:07:15 +0530

    Updates

---
+++ precache-manifest.cdb79fa78dd3a8e10d4e25f892869e94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/venue-website/static/media/slide_two.3a3ffd36.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/venue-website/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/venue-website/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "7ec715474311ee24139c",
+    "url": "/venue-website/static/js/runtime~main.7ec71547.js"
+  },
+  {
+    "revision": "0605ad79abdffca8aa12",
+    "url": "/venue-website/static/js/main.0605ad79.chunk.js"
+  },
+  {
+    "revision": "4eafe5056c9fd0fd6481",
+    "url": "/venue-website/static/js/1.4eafe505.chunk.js"
+  },
+  {
+    "revision": "0605ad79abdffca8aa12",
+    "url": "/venue-website/static/css/main.c724e46c.chunk.css"
+  },
+  {
+    "revision": "afac186e7a28920171360e7b6bbdd1a0",
+    "url": "/venue-website/index.html"
+  }
+];
